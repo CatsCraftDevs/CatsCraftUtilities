2e0c49e0c38e71fb9f97290002b6aa7afe2328bd
brainsynder <briansnyder498@gmail.com>
2021-10-31 15:20:42 -0400

    Initial commit

---
+++ src/main/java/utilities/catscraft/CatsCraftUtilities.java
@@ -0,0 +1,17 @@
+package utilities.catscraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CatsCraftUtilities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
